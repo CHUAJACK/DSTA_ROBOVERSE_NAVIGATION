ff5f2458b437405237009f5ca9e787bafd4b9ca4
CHUAJACK <wjackch36@gmail.com>
2026-05-12 23:54:23 +0800

    odom broadcaster

--- tf2_static_transformer_launch_file.py
+++ tf2_static_transformer_launch_file.py
@@ -6,23 +6,6 @@ def generate_launch_description():
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
-            name='base_link_to_odom',
-            arguments=[
-                '--x', '0',
-                '--y', '0',
-                '--z', '0',
-                '--qx', '0',
-                '--qy', '0',
-                '--qz', '0',
-                '--qw', '1',
-                '--frame-id', 'odom',
-                '--child-frame-id', 'base_link'
-            ]
-        ),
-
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
             name='camera_to_base_link',
             arguments=[
                 '--x', '0.12',
@@ -36,15 +19,14 @@ def generate_launch_description():
                 '--child-frame-id', 'camera_link'
             ]
         ),
-
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
             name='odom_to_map',
             arguments=[
-                '--x', '0',
-                '--y', '0',
-                '--z', '0',
+                '--x', '0.0',
+                '--y', '0.0',
+                '--z', '0.0',
                 '--qx', '0',
                 '--qy', '0',
                 '--qz', '0',
@@ -53,4 +35,19 @@ def generate_launch_description():
                 '--child-frame-id', 'odom'
             ]
         ),
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            name='gz_ros_bridge_points',
+            output='screen',
+            parameters=[{
+                # You can also pass a yaml file here, but for a single bridge, 
+                # we use the 'config' or arguments approach.
+            }],
+            arguments=[
+                # The syntax: /topic@ros_msg@gz_msg[direction]
+                '/depth_camera/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked'
+            ]
+        )
+    
     ])
